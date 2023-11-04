c22e33c82e8fc897c42e15719d8513681ca37a44
Mohamed ElHariri <mohamed-elhariri@hotmail.com>
2023-11-04 22:25:53 +0200

    Added horizontal movement to the pc
    pc = player character

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,17 +7,27 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float characterSpeed = 10f;
     Vector2 moveInput;
+    Rigidbody2D myRigidBody;
+    
 
     void Start()
     {
+        myRigidBody = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2 (moveInput.x * characterSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
     }
 
     void OnMove(InputValue value)

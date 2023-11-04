44cc3d0d74dd30bbb76e49a1f6cfb33e845ae578
Mohamed ElHariri <mohamed-elhariri@hotmail.com>
2023-11-04 23:21:02 +0200

    Added the ability to jump to the pc
    use the space button makes the player character jumps

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float characterSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
     Vector2 moveInput;
     Rigidbody2D myRigidBody;
     Animator myAnimator;
@@ -51,5 +52,13 @@ public class PlayerMovement : MonoBehaviour
     {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
+    }
+
+    void OnJump(InputValue value)
+    {
+        if(value.isPressed)
+        {
+            myRigidBody.velocity += new Vector2 (0f, jumpSpeed);
+        }
     }
 }

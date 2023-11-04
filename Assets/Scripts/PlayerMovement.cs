55c5558a03b71a91f64c9fddc646e3984dc1b5ca
Mohamed ElHariri <mohamed-elhariri@hotmail.com>
2023-11-04 22:50:56 +0200

    Added code to flip the pc
    pc = player character

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -22,6 +22,18 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         Run();
+        FlipSprite();
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        
+        if(playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2 (Mathf.Sign(myRigidBody.velocity.x),1f);
+        }
+        
     }
 
     void Run()
